58a4a06ef06655e571e479de17d929d2f4c1d45d
Lud Serapio <serapio.ludwing.gt@gmail.com>
2025-03-24 03:00:52 -0600

    implement request scope for log requestId

--- src/shared/logger/app.logger.ts
+++ src/shared/logger/app.logger.ts
@@ -1,24 +1,42 @@
-import { ConsoleLogger, Injectable } from '@nestjs/common';
+import { ConsoleLogger, Inject, Injectable, Scope } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class AppLogger extends ConsoleLogger {
-  log(message: string, context?: string) {
-    super.log(message, context);
-  }
+    constructor(
+        @Inject(REQUEST) private request: Request
+    ){
+        super();
+    }
 
-  error(message: string, trace?: string, context?: string) {
-    super.error(message, trace, context);
-  }
+    log(message: string, context?: string) {
+        const requestId = this.request['requestId'];
+        message = `ID [${requestId}]: ${message}`;
+        super.log(message, context);
+    }
 
-  warn(message: string, context?: string) {
-    super.warn(message, context);
-  }
+    error(message: string, trace?: string, context?: string) {
+        const requestId = this.request['requestId'];
+        message = `ID [${requestId}]: ${message}`;
+        super.error(message, trace, context);
+    }
 
-  debug(message: string, context?: string) {
-    super.debug?.(message, context); // Optional, depending on config
-  }
+    warn(message: string, context?: string) {
+        const requestId = this.request['requestId'];
+        message = `ID [${requestId}]: ${message}`;
+        super.warn(message, context);
+    }
 
-  verbose(message: string, context?: string) {
-    super.verbose?.(message, context);
-  }
+    debug(message: string, context?: string) {
+        const requestId = this.request['requestId'];
+        message = `ID [${requestId}]: ${message}`;
+        super.debug?.(message, context); // Optional, depending on config
+    }
+
+    verbose(message: string, context?: string) {
+        const requestId = this.request['requestId'];
+        message = `ID [${requestId}]: ${message}`;
+        super.verbose?.(message, context);
+    }
 }
