a01425a0a8c4319972c30a3fec1f198aa61ded44
Lud Serapio <serapio.ludwing.gt@gmail.com>
2025-03-24 02:25:06 -0600

    implement capacitor for ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.forzafront.com',
+  appName: 'forza-front',
+  webDir: 'www'
+};
+
+export default config;
